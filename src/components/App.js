f7ea7a891e3b3c2122bab437be7bc9997481794d
Saty4Dev <57029286+Saty4Dev@users.noreply.github.com>
2022-02-16 02:10:35 +0530

    First

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import ProjComp from "./ProjComp";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <ProjComp />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/ProjComp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProjComp = () => {
+  <>
+    <h1 data-ns-test="project-name"> Logistics Web App</h1>
+    <p data-ns-test="project-description">
+      A web based application that would take care of requisitions raised by
+      shipping vessels across the globe. The app would effectively have to
+      manage the client’s logistics supply chain and would also have to
+      accommodate forwarding and tracking of goods carried by the vessel.
+    </p>
+  </>;
+};
+export default ProjComp;
